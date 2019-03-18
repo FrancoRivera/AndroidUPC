f1ef202ae61cd9c08a19d3b595f88d121e8f9a6e
Alumnos <alumnos@B606-05.upc.edu.pe>
2019-03-18 10:17:57 -0500

    initial commit

---
+++ RestCountries/app/src/main/java/com/example/restcountries/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.restcountries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //AndroidNetworking.initialize(getApplicationContext());
+        AndroidNetworking.get("https://api.kanye.rest")
+                .addPathParameter("pageNumber", "0")
+                .addQueryParameter("limit", "3")
+                .addHeaders("token", "1234")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // do anything with response
+                        System.out.println(response);
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                    }
+                });
+    }
+}
