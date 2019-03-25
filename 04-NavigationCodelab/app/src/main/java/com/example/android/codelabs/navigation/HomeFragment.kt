13a4b8375d1a614c8f67f00c6e20ab1be9f819f2
Franco Rivera <franco@riverarivas.com>
2019-03-25 08:57:48 -0500

    04 finished step 07

--- 04-NavigationCodelab/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ 04-NavigationCodelab/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 
@@ -39,7 +40,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO STEP 6 - Set NavOptions
         val options = navOptions {
             anim {
                 enter = R.anim.slide_in_right
@@ -51,13 +51,11 @@ class HomeFragment : Fragment() {
         view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
             findNavController().navigate(R.id.flow_step_one_dest, null, options)
         }
-        //TODO END STEP 6
 
-        //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
-//        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
-//                Navigation.createNavigateOnClickListener(R.id.next_action, null)
-//        )
-        //TODO END STEP 7.2
+        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.next_action, null)
+        )
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
