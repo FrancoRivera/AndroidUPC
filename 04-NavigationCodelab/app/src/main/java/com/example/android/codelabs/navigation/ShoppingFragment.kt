ed28302bb363ed03cb3139442073867d170a166b
Franco Rivera <franco@riverarivas.com>
2019-04-25 13:29:03 -0500

    04 finish codelab

--- 04-NavigationCodelab/app/src/main/java/com/example/android/codelabs/navigation/ShoppingFragment.kt
+++ 04-NavigationCodelab/app/src/main/java/com/example/android/codelabs/navigation/ShoppingFragment.kt
@@ -9,15 +9,13 @@ import androidx.fragment.app.Fragment
 /**
  * A simple [Fragment] subclass.
  */
-class SettingsFragment : Fragment() {
+class ShoppingFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+        return inflater.inflate(R.layout.shopping_fragment, container, false)
     }
+
 
 }
